96444f4b4fd9ce435a0ba178ad31f27b9baea74b
Stefan <steve232np@gmail.com>
2020-10-16 11:11:46 +0200

    follow eslint rules

--- index.test.js
+++ index.test.js
@@ -1,42 +1,44 @@
-const generateUniqueID = require('./index')
+const generateUniqueID = require('./index');
 
 test('default behavior', () => {
-  const id1 = generateUniqueID()
-  const id2 = generateUniqueID()
+  const id1 = generateUniqueID();
+  const id2 = generateUniqueID();
 
-  expect(id1.length).toBe(20)
-  expect(id1).not.toMatch(id2)
-})
+  expect(id1.length).toBe(20);
+  expect(id1).not.toMatch(id2);
+});
 
 test('use only letters', () => {
-  const id = generateUniqueID({ useNumbers: false, length: 50 })
+  const id = generateUniqueID({ useNumbers: false, length: 50 });
 
-  expect(id.match(/[0-9]/g)).toBe(null)
-  expect(id.length).toBe(50)
-})
+  expect(id.match(/[0-9]/g)).toBe(null);
+  expect(id.length).toBe(50);
+});
 
 test('use only numbers', () => {
-  const id = generateUniqueID({ useLetters: false, length: 30 })
+  const id = generateUniqueID({ useLetters: false, length: 30 });
 
-  expect(id.match(/[a-z]/g)).toBe(null)
-  expect(id.length).toBe(30)
-})
+  expect(id.match(/[a-z]/g)).toBe(null);
+  expect(id.length).toBe(30);
+});
 
 test('include symbols', () => {
-  let id = generateUniqueID({ includeSymbols: ['@', '#'], length: 100 })
+  let id = generateUniqueID({ includeSymbols: ['@', '#'], length: 100 });
 
-  expect(((/(@|#)/g)).test(id)).toBe(true)
+  expect(((/(@|#)/g)).test(id)).toBe(true);
 
-  id = generateUniqueID({ includeSymbols: ['@', '#'], length: 10, useLetters: false, useNumbers: false })
+  id = generateUniqueID({
+    includeSymbols: ['@', '#'], length: 10, useLetters: false, useNumbers: false,
+  });
 
-  expect(id.match(/[a-z0-9]/g)).toBe(null)
-  expect(id.includes('@')).toBe(true)
-  expect(id.includes('#')).toBe(true)
-  expect(id.length).toBe(10)
-})
+  expect(id.match(/[a-z0-9]/g)).toBe(null);
+  expect(id.includes('@')).toBe(true);
+  expect(id.includes('#')).toBe(true);
+  expect(id.length).toBe(10);
+});
 
 test('exclude symbols', () => {
-  const id = generateUniqueID({ useLetters: false, excludeSymbols: ['0', '1', '2', '3', '4', '5'] })
+  const id = generateUniqueID({ useLetters: false, excludeSymbols: ['0', '1', '2', '3', '4', '5'] });
 
-  expect(id.match(/[0-5]/g)).toBe(null)
-})
+  expect(id.match(/[0-5]/g)).toBe(null);
+});
