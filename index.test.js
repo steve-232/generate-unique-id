325fee10009022c2cf5c325141d5e9366e283944
Stefan <steve232np@gmail.com>
2023-07-31 19:27:54 +0200

    refactor unit-test functions

--- index.test.js
+++ index.test.js
@@ -1,44 +1,62 @@
 const generateUniqueId = require('./index');
 
-test('default behavior', () => {
-  const id1 = generateUniqueId();
-  const id2 = generateUniqueId();
+describe('ID', () => {
+  it('should be generated without passing any additional arguments', () => {
+    const id1 = generateUniqueId();
+    const id2 = generateUniqueId();
 
-  expect(id1.length).toBe(20);
-  expect(id1).not.toMatch(id2);
-});
-
-test('use only letters', () => {
-  const id = generateUniqueId({ useNumbers: false, length: 50 });
-
-  expect(id.match(/[0-9]/g)).toBe(null);
-  expect(id.length).toBe(50);
-});
-
-test('use only numbers', () => {
-  const id = generateUniqueId({ useLetters: false, length: 30 });
-
-  expect(id.match(/[a-z]/g)).toBe(null);
-  expect(id.length).toBe(30);
-});
-
-test('include symbols', () => {
-  let id = generateUniqueId({ includeSymbols: ['@', '#'], length: 100 });
-
-  expect(((/(@|#)/g)).test(id)).toBe(true);
-
-  id = generateUniqueId({
-    includeSymbols: ['@', '#'], length: 10, useLetters: false, useNumbers: false,
+    expect(id1.length).toBe(20);
+    expect(id1).not.toMatch(id2);
   });
 
-  expect(id.match(/[a-z0-9]/g)).toBe(null);
-  expect(id.includes('@')).toBe(true);
-  expect(id.includes('#')).toBe(true);
-  expect(id.length).toBe(10);
-});
+  it('should contain only letters', () => {
+    const id = generateUniqueId({
+      useNumbers: false,
+      length: 50,
+    });
 
-test('exclude symbols', () => {
-  const id = generateUniqueId({ useLetters: false, excludeSymbols: ['0', '1', '2', '3', '4', '5'] });
+    expect(id.match(/[0-9]/g)).toBe(null);
+    expect(id.length).toBe(50);
+  });
 
-  expect(id.match(/[0-5]/g)).toBe(null);
+  it('should contain only numbers', () => {
+    const id = generateUniqueId({
+      useLetters: false,
+      length: 30,
+    });
+
+    expect(id.match(/[a-z]/g)).toBe(null);
+    expect(id.length).toBe(30);
+  });
+
+  it('should include certain symbols', () => {
+    const id = generateUniqueId({
+      includeSymbols: ['@', '#'],
+      length: 100,
+    });
+
+    expect(((/(@|#)/g)).test(id)).toBe(true);
+    expect(id.length).toBe(100);
+
+    const id2 = generateUniqueId({
+      includeSymbols: ['@', '#'],
+      length: 10,
+      useLetters: false,
+      useNumbers: false,
+    });
+
+    expect(id2.match(/[a-z0-9]/g)).toBe(null);
+    expect(id2.includes('@')).toBe(true);
+    expect(id2.includes('#')).toBe(true);
+    expect(id2.length).toBe(10);
+  });
+
+  it('should exclude certain symbols', () => {
+    const id = generateUniqueId({
+      useLetters: false,
+      excludeSymbols: ['0', '1', '2', '3', '4', '5'],
+    });
+
+    expect(id.match(/[0-5]/g)).toBe(null);
+  });
 });
